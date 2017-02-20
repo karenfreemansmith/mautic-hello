1f07c6093970133e71c80fd4f4f8ebadfcfc2c8d
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-20 13:26:58 -0800

    basic plugin boilerplate

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'name' => 'Hello',
+  'description' => 'Most Basic Plugin',
+  'author' => 'Karen Freeman-Smith',
+  'version' => '1.0.0'
+);
+
+ ?>
---
+++ HelloBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace
+MauticPlugin\HelloBundle
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class HelloBundle extends PluginBundleBase {
+  // make it say "Hello World" somewhere?
+}
+ ?>
